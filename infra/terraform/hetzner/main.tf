70dd831867ae0c7807b0e55ab8c6846ff3e9752b
filippo-mancinelli <filipp.mancinelli@studenti.unicam.it>
2025-02-09 18:21:00 +0100

    Terraform, dynamically saving SSH key to cloud HCP Vault as secret

--- infra/terraform/hetzner/main.tf
+++ infra/terraform/hetzner/main.tf
@@ -1,18 +1,54 @@
 terraform {
+  cloud {
+    organization = "maronnodes"
+
+    workspaces {
+      name = "HCPVaultSecretsLab"
+    }
+  }
   required_providers {
     hcloud = {
       source  = "hetznercloud/hcloud"
       version = "~> 1.42.0"
     }
+    hcp = {
+      source = "hashicorp/hcp"
+      version = "0.91.0"
+    }
   }
+}
+
+data "hcp_vault_secrets_app" "web_application" {
+  app_name = "maronnodes"
 }
 
 variable "hcloud_token" {
   sensitive = true
 }
 
+variable "vault_token" {
+  sensitive = true
+}
+
+variable "hcp_client_id" {
+  sensitive = true
+}
+
+variable "hcp_client_secret" {
+  sensitive = true
+}
+
+variable "hcp_project_id" {
+  sensitive = false
+}
+
 provider "hcloud" {
   token = var.hcloud_token
+}
+
+provider "hcp" {
+  client_id     = var.hcp_client_id
+  client_secret = var.hcp_client_secret
 }
 
 # Generate SSH key for Ansible
@@ -28,8 +64,8 @@ resource "hcloud_ssh_key" "ansible" {
 # Create server with Docker pre-installed via cloud-init
 resource "hcloud_server" "polygon_node" {
   name        = "polygon-node-${formatdate("YYYYMMDDhhmmss", timestamp())}"
-  image       = "ubuntu-22.04"
-  server_type = "cx11"  # 2 vCPU, 2GB RAM (sufficient for Polygon testnet)
+  image       = "ubuntu-24.04"
+  server_type = "cx22"
   location    = "nbg1"
   ssh_keys    = [hcloud_ssh_key.ansible.id]
 
@@ -51,4 +87,9 @@ resource "hcloud_server" "polygon_node" {
 # Output server IP for Ansible
 output "server_ip" {
   value = hcloud_server.polygon_node.ipv4_address
+}
+
+output "secrets" {
+  value = data.hcp_vault_secrets_app.web_application.secrets
+  sensitive = true
 }
